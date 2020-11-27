19d69aa85ed689dd9304a26d952c0fb61eac286f
marcosravagnani <72414608+marcosravagnani@users.noreply.github.com>
2020-11-27 18:54:44 -0300

    Start Commit

--- app/src/main/java/com/example/projeto_smanet/tela_login.java
+++ app/src/main/java/com/example/projeto_smanet/tela_login.java
@@ -1,5 +1,6 @@
 package com.example.projeto_smanet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class tela_login extends AppCompatActivity {
     Button btn_entrar;
     EditText edt_login, edt_senha;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +32,50 @@ public class tela_login extends AppCompatActivity {
         edt_senha = (EditText) findViewById(R.id.edt_senha);
         btn_entrar = (Button) findViewById(R.id.btn_entrar);
 
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edt_login.getText().toString().trim();
+                String password = edt_senha.getText().toString().trim();
+
+                loginMethod(email, password);
+            }
+        });
+
     }
-//valida login e senha
+
+    private void loginMethod(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(tela_login.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent i = new Intent(tela_login.this, MainActivity.class);
+                    startActivity(i);
+                }else{
+                    alert("Email ou senha incorretos");
+                }
+            }
+        });
+
+    }
+
+    private void alert(String email_ou_senha_incorretos) {
+        Toast.makeText(tela_login.this, email_ou_senha_incorretos, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = conexaoBD.getFirebaseAuth();
+        user = conexaoBD.getFirebaseUser();
+    }
+
+
+/*valida login e senha
     public void valida (View view) {
         if (edt_login.getText().toString().equals("admin") && edt_senha.getText().toString().equals("admin"))
             startActivity(new Intent(tela_login.this, MainActivity.class));
         else
             Toast.makeText(this, "usuário ou senha inválido", Toast.LENGTH_LONG).show();
-    }
+    }*/
 }
