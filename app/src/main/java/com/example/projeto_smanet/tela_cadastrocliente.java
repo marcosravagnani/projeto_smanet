ca557ee44fb56cae0c6eafa088be49d9ce2bd923
marcosravagnani <72414608+marcosravagnani@users.noreply.github.com>
2020-11-30 03:51:43 -0300

    Start Commit

--- app/src/main/java/com/example/projeto_smanet/tela_cadastrocliente.java
+++ app/src/main/java/com/example/projeto_smanet/tela_cadastrocliente.java
@@ -8,22 +8,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class tela_cadastrocliente extends AppCompatActivity {
 
     private Button tela_contrato, tela_principal, btn_cadastrar;
     private EditText edt_nome, edt_cpf, edt_telefone;
+
+    //info dados cliente
+    private String Nome, cpf, telefone;
+
+    //conexão com bd
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastrocliente);
 
-        EditText edt_nome = (EditText) findViewById(R.id.edt_nomecliente);
-        EditText edt_cpf = (EditText) findViewById(R.id.edt_cpfcliente);
-        EditText edt_telefone = (EditText) findViewById(R.id.edt_telefone);
+        final EditText edt_nome = (EditText) findViewById(R.id.edt_nomecliente);
+        final EditText edt_cpf = (EditText) findViewById(R.id.edt_cpfcliente);
+        final EditText edt_telefone = (EditText) findViewById(R.id.edt_telefone);
         Button tela_contrato = (Button)findViewById(R.id.btn_criarcontrato);
         Button tela_principal = (Button)findViewById(R.id.btn_voltarmenu);
         Button btn_cadastrar = (Button)findViewById(R.id.btn_cadastrar);
+
+        inicializarFirebase();
+
+        //evento onclick para dados no bd
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               dadosCliente(edt_cpf.getText().toString(), edt_nome.getText().toString(), edt_telefone.getText().toString());
+            }
+        });
 
         //chama tela de cadastro de contrato
         tela_contrato.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +62,53 @@ public class tela_cadastrocliente extends AppCompatActivity {
             }
         });
     }
+    private void dadosCliente(String nome, String cpf, String telefone){
+        tela_cadastrocliente tc = new tela_cadastrocliente();
+        tc.setCpf(edt_cpf.getText().toString());
+        tc.setNome(edt_nome.getText().toString());
+        tc.setTelefone(edt_telefone.getText().toString());
+        databaseReference.child("Cliente").child(tc.getCpf()).setValue(tc);
+    }
 
+    private void inicializarFirebase(){
+        FirebaseApp.initializeApp(tela_cadastrocliente.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    //método construtor da classe
+    public tela_cadastrocliente() {
+
+    }
+
+//get and set
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    @Override
+    public String toString(){
+        return cpf;
+    }
 
 
 }
